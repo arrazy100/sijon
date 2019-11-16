dff74a90b1f47836f9e152e67f3610dfd4353401
arrazy100 <afdhalarrazy111@gmail.com>
2019-11-16 15:40:08 +0700

    Little update

--- application/controllers/admin/Users.php
+++ application/controllers/admin/Users.php
@@ -2,7 +2,7 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Blogs extends CI_Controller
+class Users extends CI_Controller
 {
     public function __construct()
     {
--- application/views/admin/_partials/sidebar.php
+++ application/views/admin/_partials/sidebar.php
@@ -6,7 +6,7 @@
             <span>Overview</span>
         </a>
     </li>
-    <li class="nav-item dropdown <?php echo $this->uri->segment(2) == 'products' ? 'active': '' ?>">
+    <li class="nav-item dropdown <?php echo $this->uri->segment(2) == 'users' ? 'active': '' ?>">
         <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
             aria-expanded="false">
             <i class="fas fa-fw fa-user"></i>
@@ -15,6 +15,17 @@
         <div class="dropdown-menu" aria-labelledby="userDropdown">
             <a class="dropdown-item" href="<?php echo site_url('admin/users/add') ?>">New User</a>
             <a class="dropdown-item" href="<?php echo site_url('admin/users') ?>">List User</a>
+        </div>
+    </li>
+    <li class="nav-item dropdown <?php echo $this->uri->segment(2) == 'blogs' ? 'active': '' ?>">
+        <a class="nav-link dropdown-toggle" href="#" id="blogDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
+            aria-expanded="false">
+            <i class="fas fa-fw fa-user"></i>
+            <span>Blogs</span>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="blogDropdown">
+            <a class="dropdown-item" href="<?php echo site_url('admin/blogs/add') ?>">New User</a>
+            <a class="dropdown-item" href="<?php echo site_url('admin/blogs') ?>">List User</a>
         </div>
     </li>
     <li class="nav-item">

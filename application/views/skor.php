3c25aee6ed4237e769e4386f3354558b237d4eb9
arrazy100 <afdhalarrazy111@gmail.com>
2019-11-20 14:36:08 +0700

    Testing

--- application/views/skor.php
+++ application/views/skor.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Hasil Skor</title>
+    <title>Perhitungan Skor</title>
 </head>
 <body>
     <?php if ($skor > 50) {
-        ?> <h1>Selamat Anda Berhasil</h1>
+        ?> <h4>Selamat Anda Berhasil</h1>
     <?php } else { ?>
             <h1>Anda Gagal</h1>
     <?php } ?>
